51794073ab3dc204caf5221d67fb6898b585cd46
Matthew McClure <mcclure0728@gmail.com>
2017-03-20 18:37:03 -0700

    Create faqsMain.js

---
+++ components/faqs/faqsMain.js
@@ -0,0 +1,47 @@
+/* global angular */
+angular.module('public.faqs')
+    .component('faqsMain', {
+        bindings: {
+            count: '='
+        },
+        templateUrl: '/client/aboutUs/components/templates/faqsmain.tpl.html'
+    })
+
+FaqsMainController.$inject = ['$state', 'FaqsService']
+
+function FaqsMainController($state, FaqsService) {
+    var vm = this
+    vm.faqLine = 'Frequently Asked Questions'
+    vm.faqs = []
+    vm.sellFaqs = []
+    vm.buyFaqs = []
+    vm.questionTypes = ['general', 'seller', 'buyer']
+
+    getAllFaqs()
+
+    function getAllFaqs() {
+        FaqsService.getAll(getAllSuccess, getAllError)
+    }
+
+    function getAllSuccess(data, sellerData, buyerData) {
+        for (var i = 0; i < data.items.length; i++) {
+            var currentFaq = data.items[i] // added items
+            switch (currentFaq.questionType) {
+                case 'general':
+                    vm.faqs.push(currentFaq)
+                    break
+
+                case 'seller':
+                    vm.sellFaqs.push(currentFaq)
+                    break
+
+                case 'buyer':
+                    vm.buyFaqs.push(currentFaq)
+            }
+        }
+    }
+
+    function getAllError(err) {
+        console.log(err)
+    }
+}
